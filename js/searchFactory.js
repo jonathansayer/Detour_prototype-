89bb198bac25db4a55a80927a66e754aafc6a40e
jonathansayer <jon.sayer1@gmail.com>
2015-08-11 16:09:43 +0100

    search for churches/retaurants and museums

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -2,14 +2,14 @@ parkSearch.factory('Search', ['$http', function($http) {
   var queryUrl = 'https://maps.googleapis.com/maps/api/place/radarsearch/json?';
 
   return {
-    query: function() {
+    query: function(searchTerm) {
       return $http({
         url: queryUrl,
         method: 'GET',
         params: {
           'location': '51.5191120,-0.0745060',
           'radius': '1000',
-          'types': 'park',
+          'types': searchTerm,
           'key': ''
         }
       });
