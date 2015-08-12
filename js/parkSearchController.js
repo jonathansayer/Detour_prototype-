be4656cb5b4822fe182789a9c190b6e548506b23
jonathansayer <jon.sayer1@gmail.com>
2015-08-12 14:09:56 +0100

    working map

--- js/parkSearchController.js
+++ js/parkSearchController.js
@@ -1,6 +1,7 @@
 parkSearch.controller('ParkSearchController', ['Search', function(Search) {
 
   var self = this;
+  var map;
 
   self.doSearch = function() {
     Search.query(self.searchTerm)
@@ -8,4 +9,14 @@ parkSearch.controller('ParkSearchController', ['Search', function(Search) {
         self.searchResult = response.data;
       })
   };
+
+  self.initMap = function() {
+    console.log("hello")
+    self.map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 51.5191120, lng: -0.0745060},
+      zoom: 15
+    });
+  }
+
+
 }]);
